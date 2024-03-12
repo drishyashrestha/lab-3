eefae0c1e4694174e4ce8ee5f8edd5019834cf07
drishyashrestha <shresthadrishya06@gmail.com>
2024-03-11 22:23:17 -0400

    lab part 1

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Drishya Shrestha on 3/5/24.
+//
+// You're almost always going to need this when your referencing UI elements in your file
+import UIKit
+
+class ForecastViewController: UIViewController {
+    
+
+    // Function override for the view controller
+    // This is fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        // Some functions require you to call the super class implementation
+        // Always read the online documentation to know if you need to
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
